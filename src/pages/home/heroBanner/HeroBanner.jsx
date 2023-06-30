9e2cec7d32d989632d587d8e013b9b2c695f6504
ShashiS07 <shashiss0708@gmail.com>
2023-06-30 20:37:32 +0530

    error fixed

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import "./style.scss";
+import "./HeroBanner.scss";
 
 import useFetch from "../../../hooks/useFetch";
 
